625e6349d000aac3432f4c46fd422d3a20349923
chelcee-xx <chelcee1614@gmail.com>
2022-06-23 11:23:41 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(newEntry =>
+        <Note 
+          key = {newEntry.key}
+          title = {newEntry.title}
+          content = {newEntry.content}
+        />)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
